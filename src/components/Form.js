929dd6fa2de1952f2d6bb3d61ed8b590a4aafd5a
phazhion <phazhion@gmail.com>
2019-07-14 09:46:51 -0600

    Finished Form Component

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,36 +2,42 @@ import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
 
 class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isEmailValid: false,
-            isNameValid: false,
-            isPhoneValid: false,
-            isUrlValid: false,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEmailValid: false,
+      isNameValid: false,
+      isPhoneValid: false,
+      isUrlValid: false,
+    };
+  }
 
-    }
-    
-    render() {
-        return (
-            <div className="row">
-            <h1 className="text-center">Form Validation</h1>
-            <form>
-                <h3>Name:
-                </h3>
-                <h3>Email:
-                </h3>
-                <h3>Phone:
-                </h3>
-                <h3>Blog URL:
-                </h3>
-                <div className="small-6 small-centered text-center columns">
-                    <a href="#" className="button success expand round text-center">Verify</a>
-                </div>
-            </form>
-        </div>);
-    }
+  render() {
+    return (
+      <div className="row">
+        <h1 className="text-center">Form Validation</h1>
+        <form>
+          <h3>Name:</h3>
+          <input type="text" placeholder="Enter your name" name="name" />
+          <h3>Email:</h3>
+          <input type="text" placeholder="Enter your email" name="email" />
+          <h3>Phone:</h3>
+          <input
+            type="text"
+            placeholder="Enter your phone number"
+            name="phone"
+          />
+          <h3>Blog URL:</h3>
+          <input type="text" placeholder="Enter your blog URL" name="link" />
+          <div className="small-6 small-centered text-center columns">
+            <a href="#" className="button success expand round text-center">
+              Verify
+            </a>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Form;
