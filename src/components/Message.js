22783c7b5d35efab451f8b6fdd1cf127b345b918
phazhion <phazhion@gmail.com>
2019-07-14 09:48:16 -0600

    Updated Message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Message = () => {
-    return (
-        <div>
-            <h3 className="text-center message"></h3>
-        </div>
-    )
-}
+const Message = props => {
+  return (
+    <div>
+      <h3 className="text-center message">Form is {props.status}!</h3>
+    </div>
+  );
+};
 
 export default Message;
