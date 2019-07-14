87d1ae7d6b614873cd6573f014fbd38cf93ddaaa
phazhion <phazhion@gmail.com>
2019-07-14 10:43:57 -0600

    Added url validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -40,6 +40,12 @@ class Form extends Component {
     );
   };
 
+  validateUrl = () => {
+    return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/.test(
+      this.state.url,
+    );
+  };
+
   validateClickHandler = e => {
     e.preventDefault();
 
@@ -47,6 +53,7 @@ class Form extends Component {
       isNameValid: this.validateName(),
       isPhoneValid: this.validatePhone(),
       isEmailValid: this.validateEmail(),
+      isUrlValid: this.validateUrl(),
     });
   };
 
