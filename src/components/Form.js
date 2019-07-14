c839b9fc271ef1788b85e62e83ad4927e529655e
phazhion <phazhion@gmail.com>
2019-07-14 10:09:02 -0600

    Added state values for form elements

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,8 +9,27 @@ class Form extends Component {
       isNameValid: false,
       isPhoneValid: false,
       isUrlValid: false,
+      name: '',
+      phone: '',
+      email: '',
+      url: '',
     };
+
+    this.inputOnChangeHandler = this.inputOnChangeHandler.bind(this);
   }
+
+  validateClickHandler = e => {
+    e.preventDefault();
+    console.log(e.currentTarget);
+  };
+
+  inputOnChangeHandler = e => {
+    const {name, value} = e.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
 
   render() {
     return (
@@ -18,19 +37,51 @@ class Form extends Component {
         <h1 className="text-center">Form Validation</h1>
         <form>
           <h3>Name:</h3>
-          <input type="text" placeholder="Enter your name" name="name" />
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={this.inputOnChangeHandler}
+            placeholder={
+              this.state.name === '' ? 'Enter your name' : this.state.name
+            }
+            name="name"
+          />
           <h3>Email:</h3>
-          <input type="text" placeholder="Enter your email" name="email" />
+          <input
+            type="text"
+            value={this.state.email}
+            onChange={this.inputOnChangeHandler}
+            placeholder={
+              this.state.email === '' ? 'Enter your email' : this.state.email
+            }
+            name="email"
+          />
           <h3>Phone:</h3>
           <input
             type="text"
-            placeholder="Enter your phone number"
+            value={this.state.phone}
+            onChange={this.inputOnChangeHandler}
+            placeholder={
+              this.state.phone === ''
+                ? 'Enter your phone number'
+                : this.state.phone
+            }
             name="phone"
           />
           <h3>Blog URL:</h3>
-          <input type="text" placeholder="Enter your blog URL" name="link" />
+          <input
+            type="text"
+            value={this.state.url}
+            onChange={this.inputOnChangeHandler}
+            placeholder={
+              this.state.url === '' ? 'Enter your url' : this.state.url
+            }
+            name="url"
+          />
           <div className="small-6 small-centered text-center columns">
-            <a href="#" className="button success expand round text-center">
+            <a
+              onClick={this.validateClickHandler}
+              className="button success expand round text-center">
               Verify
             </a>
           </div>
